f70c9ea44592fc123ad351424dbea8e3754c02a8
Nahid Hasan <mhasannahid15@gmail.com>
2025-12-19 19:50:38 +0600

    work with array

---
+++ array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int myNumbers[] = {25, 50, 75, 100};
+
+    // Accessing the first element (index 0)
+    printf("%d\n", myNumbers[0]); // Output: 25
+
+    // Changing the value of the third element (index 2)
+    myNumbers[2] = 90;
+    printf("%d\n", myNumbers[2]); // Output: 90
+    
+    return 0;
+}
